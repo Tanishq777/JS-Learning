5c57fc49b2f9b13263366339227e9f9e6639251d
Tanishq <52687608+Tanishq777@users.noreply.github.com>
2025-10-12 13:41:35 +0000

    variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,8 @@
+let city = "Delhi"
+const code = 1425 
+username = "Spiderman" // not recommended
+var password = "1234" // not recommended
+
+city = "Bengaluru"
+username = "Iron Man"
+console.table([city,code, username])
