bd824d7dd0db7ab01cf368fc24f1a7bae2572e3b
Tanishq <52687608+Tanishq777@users.noreply.github.com>
2025-10-13 12:54:27 +0000

    datatypes in js

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" // treat all JS code written in newer version
+
+/*
+number => 2^53 range
+bigInt => Even Bigger Range
+string => ""
+boolean => true/false
+null => Standalone Object
+undefined => value not assigned
+symbol => unique
+*/
+
+console.log(typeof null) // => object
+console.log(typeof undefined) // => undefined
