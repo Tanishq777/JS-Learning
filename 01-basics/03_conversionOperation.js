00b355d1d097d2dacdf55dcb68361e1e9fb3cddb
Tanishq <52687608+Tanishq777@users.noreply.github.com>
2025-10-16 08:00:39 +0000

    conversion operations

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let val = 4
+let negval = -val
+
+console.log(negval)
+
+let s1 = "a"
+let s2 = " b"
+let s3 = s1+s2
+
+console.log(s3)
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+// not a good way to write such statements, results can be ambiguous,
+// use paranthesis instead
+
+
+
+
