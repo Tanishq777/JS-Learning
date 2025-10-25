608e28a299025c651195bb0f359a8e05db0fde96
Tanishq <52687608+Tanishq777@users.noreply.github.com>
2025-10-25 10:44:41 +0000

    more objects in js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,17 @@
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+const obj3 = {...obj1, ...obj2}
+
+console.log(obj3);
+
+// These all return arrays
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
